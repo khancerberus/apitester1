67ecf174b513393ec5cad33d089b2f7508d9b4f3
Victor Valenzuela M <vvalenzuela@itsanet>
2022-11-18 11:15:12 -0300

    Se agregan vistas de login y userviewset, se crean las url para estas

--- appTest/views.py
+++ appTest/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from appTest.serializers import UserSerializer
 
-# Create your views here.
+import requests
+import json
+
+#LOGIN
+@api_view(['POST'])
+def login(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response("Usuario inválido")
+
+    pwd_valid = check_password(password, user.password)
+
+    if not pwd_valid:
+        Response('Contraseña inválida')
+
+    token, _ = Token.objects.get_or_create(user=user)
+
+    return Response(token.key)
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/1").text
+    resource = json.loads(response)
+    print("Titulo" + resource['title'])
